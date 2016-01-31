d6c1a1346b6b67246e4772a9bd7a655dd608c1ea
nsarafa <nicksarafa@gmail.com>
2016-01-31 03:22:47 -0500

    routes logo-icon click to root index view

--- client/templates/layouts/appLayout.js
+++ client/templates/layouts/appLayout.js
@@ -1,3 +1,9 @@
 Template.appLayout.rendered = function () {
   Session.set('currentTab', 'trending');
 };
+
+Template.appLayout.events({
+  'click .logo-icon': function (event, template) {
+    Router.go('/');
+  }
+});
